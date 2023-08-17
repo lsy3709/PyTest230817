fbe72df46888cbc8e6aecb9174ead78cedfca336
이상용 <lsy3709@naver.com>
2023-08-17 11:33:44 +0900

    230817 3교시 저장1

--- 230817/test.py
+++ 230817/test.py
@@ -1,1 +1,19 @@
+# input() : 터미널에서 , 문자열을 입력하는 형식.
+print("이메일을 입력해주세요 ")
+email = input()
+print(f"입력된 이메일은 : {email}")
+
+
 print("hello python")
+# ctrl + f5
+a = 1
+print(type(a))
+b = str(a)
+print(b)
+print(type(b))
+# 출력 형식, f 포맷 스트링 .
+print(f"test a출력: {a}")
+# 코틀린 로그캣 출력.
+# Log.d("lsy","출력하기 a : ${a}")
+
+print("출력형식 %d " % (a))
