c3600c93f945f1fc5ac1db16bf160e1597db4b74
leeos20k <leeos20k@naver.com>
2023-08-21 12:02:41 +0900

    0821이영석

--- homework/230818/230818_lys/test.py
+++ homework/230818/230818_lys/test.py
@@ -6,12 +6,11 @@ print(f"yesterday_lyric의 내용 출력 부분 : {yesterday_lyric}")
 
 f.close()
 
-
 contents = ""
 
 for line in yesterday_lyric:
     contents = contents + line.strip() + "\n"
-   
+
 print(f"contents의 내용이 궁금 하면 반복문 다 종료후 : {contents}")
 
 result = contents.upper().count("YESTERDAY")
